0d1d880e99e2853debab0020afca9568539b6eae
bengisukayan <bengisukayan@gmail.com>
2024-12-01 22:36:20 +0300

    Walking established for player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 8;
+    public float groundDistance = 0;
+
+    public LayerMask floor;
+    public Rigidbody rigidBody;
+    public SpriteRenderer spriteRenderer;
+    private bool _walking;
+    public Animator animator;
+
+
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, floor))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDistance;
+                transform.position = movePos;
+            }
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moveDir = new(moveHorizontal, 0, moveVertical);
+        rigidBody.velocity = moveDir * speed;
+        
+        _walking = moveHorizontal != 0 || moveVertical != 0;
+        animator.SetBool("walking", _walking);
+
+        if (moveHorizontal != 0 && moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveHorizontal != 0 && moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
